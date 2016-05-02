949a80fc77b7abc1f0cadc9b89fd8ed697283847
Per Bernhardt <per@pixelhouse.de>
2016-05-02 16:56:40 +0200

    Change port to not conflict with kubernetes

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --rm -p 80:8080 -p 3001:3001 -v $(pwd):$(pwd) -w $(pwd) dr.chefkoch.net/display-pattern-lab-dev node_modules/.bin/gulp "$@"
+docker run -ti --rm -p 8181:8181 -p 3001:3001 -v $(pwd):$(pwd) -w $(pwd) dr.chefkoch.net/display-pattern-lab-dev node_modules/.bin/gulp "$@"
--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('serve', ['twig','sass', 'lab-sass', 'watch'], function() {
         server: {
             baseDir: "output"
         },
-        port: 8080,
+        port: 8181,
         ghostMode: false,
         open: false,
         host: "172.123.123.123"
