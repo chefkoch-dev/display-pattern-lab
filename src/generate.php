399e0064d6c9e6be6b5b2708701f8a8f05c8cd93
soundyogi <soundyogi@gmail.com>
2016-05-03 14:44:35 +0200

    add: nav, iframe styles

--- src/generate.php
+++ src/generate.php
@@ -2,37 +2,34 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-(function() use ($argv) {
+$config = json_decode($argv[1], true);
 
-    $config = json_decode($argv[1], true);
+$labTwigTemplatePath = realpath(__DIR__ . '/../html');
+$loader = new Twig_Loader_Filesystem($labTwigTemplatePath);
+$loader->addPath($labTwigTemplatePath, 'lab');
+foreach ($config['twig']['namespaces'] as $namespace => $path) {
+    $loader->addPath($path, $namespace);
+}
+$twig = new Twig_Environment($loader);
 
-    $labTwigTemplatePath = realpath(__DIR__ . '/../html');
-    $loader = new Twig_Loader_Filesystem($labTwigTemplatePath);
-    $loader->addPath($labTwigTemplatePath, 'lab');
-    foreach ($config['twig']['namespaces'] as $namespace => $path) {
-        $loader->addPath($path, $namespace);
-    }
-    $twig = new Twig_Environment($loader);
+$twigExtension = new \Chefkoch\DisplayPatternLab\Twig\Extension();
+$twig->addExtension($twigExtension);
 
-    $twigExtension = new \Chefkoch\DisplayPatternLab\Twig\Extension();
-    $twig->addExtension($twigExtension);
+$indexFileName = basename($config['scss']['indexFile']);
+$cssFile = '/css/' . str_replace('.scss', '.css', $indexFileName);
 
-    $indexFileName = basename($config['scss']['indexFile']);
-    $cssFile = '/css/' . str_replace('.scss', '.css', $indexFileName);
+$twig->addGlobal('displayPatternsCss', $cssFile);
 
-    $twig->addGlobal('displayPatternsCss', $cssFile);
+$factory = new \Chefkoch\DisplayPatternLab\Document\Factory();
 
-    $factory = new \Chefkoch\DisplayPatternLab\Document\Factory();
+$tree = $factory->start($config['twig']['rootDirectory']);
 
-    $tree = $factory->start($config['twig']['rootDirectory']);
-
-    file_put_contents(
-        __DIR__ . '/../output/index.html',
-        $twig->render(
-            '@lab/index.html.twig',
-            array(
-                'tree' => $tree
-            )
+file_put_contents(
+    __DIR__ . '/../output/index.html',
+    $twig->render(
+        '@lab/index.html.twig',
+        array(
+            'tree' => $tree
         )
-    );
-})();
+    )
+);
